d45e423af3fe9bf250a6a8dbb320a38d9a6eb7fd
youssefsaid997 <ysaid3641@gmail.com>
2024-09-21 04:55:07 +0300

    feat: adding home page for the portfolio and the layout

---
+++ src/frontend/views/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <main>
+      <section>Hero Section</section>
+      <section>Article</section>
+      <section>Grid about skills</section>
+      <section>carousel with the websites i worked at</section>
+    </main>
+  );
+}
+
+export default HomePage;
